e41aa833d4081e1c2ed771d7a37d07892fc6d135
Hung Nguyen <nguye3hv@mail.uc.edu>
2024-11-21 17:14:22 -0500

    function

---
+++ HttpTrigger1/index.js
@@ -0,0 +1,34 @@
+const NWS_API = 'https://api.weather.gov/';
+
+const httpMod = require('https');
+
+
+
+// Make a GET request
+fetch(apiUrl)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    const name = (req.query.name || (req.body && req.body.name));
+    const responseMessage = name
+        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: responseMessage
+    };
+}
